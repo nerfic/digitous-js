57f2ca452bfc0a8df2ea53d55c728e4b61530545
Nerfic <jdll.allan@gmail.com>
2020-12-07 12:41:07 +0100

    finish day 01

---
+++ jour01.js
@@ -0,0 +1,50 @@
+// 01 - Hello World
+
+console.log("Hello World");
+console.log("-------");
+
+// 02 - String
+
+var test = "My name is Allan";
+console.log(test);
+console.log("-------");
+
+// 03 - Concatenation
+
+var name = "Allan";
+console.log(`Nice to meet you ${name}`);
+console.log("-------");
+
+// 04 - String Length
+
+var testLength = "I'm very long !";
+console.log("Longueur de testLength =", testLength.length);
+console.log("-------");
+
+// 05 - Replace
+
+var food = "croissant is meh";
+console.log(food.replace("meh", "so good"));
+console.log("-------");
+
+// 06 - Up and Down
+
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(`basic = ${basic}, basicUp = ${basicUp}, basicDown = ${basicDown}`);
+console.log("-------");
+
+// 07 - Split
+
+var world = "banana";
+var letters = ['w', 'o', 'r', 'd'];
+console.log(letters);
+console.log("-------");
+
+// 08 - Template
+
+var age = "22";
+var template = `I'm ${age} years old`;
+console.log(template);
+console.log("-------");
