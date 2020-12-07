5ef542a432c245ded74961056ea644c5d40c0677
Nerfic <jdll.allan@gmail.com>
2020-12-07 13:57:26 +0100

    add color

--- jour01.js
+++ jour01.js
@@ -1,31 +1,31 @@
 // 01 - Hello World
 
 console.log("Hello World");
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 02 - String
 
 var test = "My name is Allan";
 console.log(test);
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 03 - Concatenation
 
 var name = "Allan";
 console.log(`Nice to meet you ${name}`);
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 04 - String Length
 
 var testLength = "I'm very long !";
 console.log("Longueur de testLength =", testLength.length);
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 05 - Replace
 
 var food = "croissant is meh";
 console.log(food.replace("meh", "so good"));
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 06 - Up and Down
 
@@ -33,21 +33,21 @@ var basic = "This is Cool";
 var basicUp = basic.toUpperCase();
 var basicDown = basic.toLowerCase();
 console.log(`basic = ${basic}, basicUp = ${basicUp}, basicDown = ${basicDown}`);
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 07 - Split
 
 var word = "banana";
 var letters = word.split()
-console.log(letters);
-console.log("-------");
+console.log("My tab =", letters);
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // 08 - Template
 
 var age = "22";
 var template = `I'm ${age} years old`;
 console.log(template);
-console.log("-------");
+console.log("\x1b[33m%s\x1b[0m", "-------");
 
 // Bonus
 
