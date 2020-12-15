697326e2c3877d3adb7b83e1f74d579e085ec36b
Nerfic <jdll.allan@gmail.com>
2020-12-15 12:53:32 +0100

    add ;

--- jour04.js
+++ jour04.js
@@ -84,7 +84,7 @@ function format(num) {
     var hours = Math.floor(num / 3600);
     var minutes = Math.floor((num - (hours * 3600)) / 60);
     var seconds = num - (hours * 3600) - (minutes * 60);
-    var hhmmss = `${hours}:${minutes}:${seconds}`
+    var hhmmss = `${hours}:${minutes}:${seconds}`;
     return hhmmss;
 }
 
