d86ed5dec39563573633e2ebfa5d4bf9c398f664
Nerfic <jdll.allan@gmail.com>
2020-12-15 15:07:44 +0100

    finish ex4 day05

---
+++ jour05-exo4.js
@@ -0,0 +1,44 @@
+const { cpuUsage } = require('process');
+var prompt = require('prompt');
+var colors = require("colors/safe");
+var mysteryNum = Math.floor(Math.random() * 100 + 1)
+console.log("Nombre mystère aléatoire", mysteryNum)
+function play() {
+
+
+    var playerResult = null;
+
+    prompt.message = colors.cyan.underline("Quel est le nombre mystère?");
+    prompt.start();
+
+    prompt.get(['number'], function (err, result) {
+
+        playerResult = result.number;
+
+        if (playerResult == mysteryNum) {
+            console.log(colors.green("Bravo!!!!!!"))
+        } else if (playerResult > mysteryNum) {
+            console.log(colors.yellow("c'est moins"))
+            play();
+            return;
+        } else if (playerResult < mysteryNum) {
+            console.log(colors.yellow("c'est plus"))
+            play();
+            return;
+        } else {
+            console.log(colors.red("Error!"))
+            play();
+            return;
+        }
+    });
+}
+
+play()
+
+// prompt.start();
+// prompt.get(['username', 'email'], function (err, result) {
+
+// console.log('Command-line input received:');
+// console.log('  username: ' + result.username);
+// console.log('  email: ' + result.email);
+// });
