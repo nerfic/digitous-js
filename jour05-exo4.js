2e22eb15b4ca4cc98ef4f91078c17bceb278de4e
Nerfic <jdll.allan@gmail.com>
2020-12-15 15:14:16 +0100

    delet commentary

--- jour05-exo4.js
+++ jour05-exo4.js
@@ -34,11 +34,3 @@ function play() {
 }
 
 play()
-
-// prompt.start();
-// prompt.get(['username', 'email'], function (err, result) {
-
-// console.log('Command-line input received:');
-// console.log('  username: ' + result.username);
-// console.log('  email: ' + result.email);
-// });
