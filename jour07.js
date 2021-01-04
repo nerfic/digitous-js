3e226d355d326af9211b67d05aa2f5189539ed00
Nerfic <jdll.allan@gmail.com>
2021-01-04 13:37:37 +0100

    add day07

--- jour07.js
+++ jour07.js
@@ -1,146 +1,35 @@
-// 01 - Alphabet
+// 01 - File System PAS FINIT
 
-function compare(a, b) {
-    console.log(a, b)
-    console.log(a.charCodeAt(0))
-    console.log(b.charCodeAt(0))
+// var fs = require('fs');
 
-    if (a.toLowerCase().charCodeAt(0) > b.toLowerCase().charCodeAt(0)) {
-        return 1;
+// fs.readFile('./jour07.txt', function (error, data) {
+//     console.log(data.toString())
+// })
+
+// 02 - Map Double
+
+var array = [1, 2, 3, 4, 5];
+var double = array.map(function (element) {
+    return element + element
+})
+
+console.log("Ex 1", double)
+
+// 03 - Map Names PAS FINIT
+
+var longNames = [
+    {
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
     }
-    else if (a.toLowerCase().charCodeAt(0) < b.toLowerCase().charCodeAt(0)) {
-        return -1
-    } else {
-        return 0;
-    }
-}
+];
 
+var shortNames = longNames.map((function (acc, element) {
+    return acc.firstName + element.lastName
+}))
 
-function sortLetters(string) {
-
-    var stringToArray = string.split("");
-    var ArrayToAlpha = stringToArray.sort(compare);
-    var AlphaToString = ArrayToAlpha.join('')
-    return AlphaToString
-
-}
-var string = "Konexio"
-
-console.log(sortLetters(string))
-console.log("\x1b[33m%s\x1b[0m", "--------------");
-
-
-
-// 02 - XOXO
-
-function countEach(string) {
-
-    var countX = 0;
-    var countO = 0;
-    var i = 0;
-    var errorMsg = "Il n'y a pas autant de x que de o "
-
-    while (i <= string.length - 1) {
-
-        if (string[i] == "x") {
-            countX++;
-        } else if (string[i] == "o") {
-            countO++;
-        }
-        i++;
-    }
-
-    if (countX === 0 && countO === 0) {
-        return errorMsg;
-    }
-    else if (countX === countO) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-var testCountEach1 = "xxxoooox"
-var testCountEach2 = "xox"
-var testCountEach3 = "aaaa"
-console.log(countEach(testCountEach1))
-console.log("\x1b[33m%s\x1b[0m", "-------");
-console.log(countEach(testCountEach2))
-console.log("\x1b[33m%s\x1b[0m", "-------");
-console.log(countEach(testCountEach3))
-console.log("\x1b[33m%s\x1b[0m", "--------------");
-
-// 03 - Palindrome
-
-function checkPal(string) {
-
-    var gg = "Palindrome!"
-    var loose = "Nope"
-    var reverseString = "";
-    var i = string.length - 1;
-
-    while (i >= 0) {
-        reverseString = reverseString + string[i];
-        i--;
-    }
-
-    if (reverseString === string) {
-        return gg;
-    } else {
-        return loose;
-    }
-}
-
-console.log("Racecar =", checkPal("racecar"))
-console.log("Laptop =", checkPal("laptop"))
-
-console.log("\x1b[33m%s\x1b[0m", "--------------");
-
-// 04 - Swap
-
-function swap(string) {
-
-    var i = 0;
-    var result = "";
-
-    while (i < string.length) {
-
-        if (string[i] === string[i].toUpperCase()) {
-            result += string[i].toLowerCase();
-        } else if (string[i] === string[i].toLowerCase()) {
-            result += string[i].toUpperCase();
-        }
-        i++;
-    }
-    return result
-}
-
-string = "Hello World";
-console.log(swap(string))
-console.log("\x1b[33m%s\x1b[0m", "--------------");
-
-// Bonus 1 => Finit
-
-function makeItSpongeBob(string) {
-
-    var i = 0;
-    result = "";
-
-    while (i < string.length) {
-
-        if (i % 2 !== 0) {
-            result += string[i].toLowerCase();
-        } else {
-            result += string[i].toUpperCase();
-        }
-        i++;
-    }
-    return result;
-}
-
-string = "Javascript is easy lol";
-console.log(makeItSpongeBob(string))
-
-
-// Bonus 2 => Finit
-// Bonus 3 => Finit
+console.log("Ex 2", shortNames)
